93353c4666e9ca6e64f2f4fa0d67699ff76da879
Mohit Sharma <mohit12_sharma@rediffmail.com>
2024-01-26 19:45:33 +1100

    Initial Commit

---
+++ iEvent/iEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  iEventApp.swift
+//  iEvent
+//
+//  Created by Mohit Sharma on 26/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct iEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
